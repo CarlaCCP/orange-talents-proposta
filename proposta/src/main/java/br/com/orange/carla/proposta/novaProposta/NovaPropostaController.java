3ff2e5d59d177aa952b3966d812e1a441f99b393
CarlaCCP <carldepaula230@gmail.com>
2021-05-12 14:37:53 -0300

    Busca proposta por identificador

--- proposta/src/main/java/br/com/orange/carla/proposta/novaProposta/NovaPropostaController.java
+++ proposta/src/main/java/br/com/orange/carla/proposta/novaProposta/NovaPropostaController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,13 +37,6 @@ public class NovaPropostaController {
 	@Autowired
 	private AnaliseProposta analise;
 	
-	@Autowired
-	private CartaoRepository cartaoRepository;
-	
-	@Autowired
-	private PostaCartao postaCartao;
-	
-
 	@PostMapping
 	public ResponseEntity<NovaPropostaResponse> cadastrar(@RequestBody @Valid NovaPropostaRequest request,
 			UriComponentsBuilder uriBuilder) {
@@ -63,20 +57,29 @@ public class NovaPropostaController {
 				}
 				if (novaProposta.getStatus() == null) {
 					novaProposta.setStatus(Status.ELEGIVEL);
-				}
-			
+				}		
 			repository.save(novaProposta);
 			URI uri = uriBuilder.path("/novaProposta/{id}").buildAndExpand(novaProposta.getId()).toUri();
 			return ResponseEntity.created(uri).body(new NovaPropostaResponse(novaProposta));
 		}
 	}
 	
-
 	@GetMapping
 	public List<NovaPropostaResponse> buscaTodos() {
 		List<NovaPropostaModel> proposta = repository.findAll();
 		
 		return NovaPropostaResponse.converter(proposta);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<NovaPropostaResponse> buscaUm (@PathVariable Long id){
+		Optional<NovaPropostaModel> proposta = repository.findById(id);
+		
+		if(proposta.isPresent()) {
+			return ResponseEntity.ok(new NovaPropostaResponse(proposta.get()));
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
 	
 }
